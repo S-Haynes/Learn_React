0df7db44b65c0938c8a11aa0198c4dfbeee7a382
Shaquille Haynes <nothintodoee@yahoo.com>
2018-07-20 04:15:22 -0400

    add seperate text component along with fake auth

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Text from './components/Text'
 
 class App extends Component {
 
   state = {
-    text: ''
+    text: '',
+    correctPass: false
   }
 
-  textChangeHandler = (event) => {
+textChangeHandler = (event) => {
     this.setState({
-      text: event.target.value
-    })
-  }
+      text: event.target.value,
+    }, this.passCheckHandler);
+ 
+}
+
+passCheckHandler = () => {
+let passCheck = [...this.state.correctPass]
+ if(this.state.text === 'puppies'){
+      passCheck = true;
+      this.setState({
+        correctPass: passCheck
+      });
+    } else {
+      passCheck = false;
+      this.setState({
+        correctPass: passCheck
+      });
+    }
+    console.log(this.state.text)
+}
+
+
   render() {
+
     return (
       <div className="App">
-      <Text textChangeHandler={this.textChangeHandler} text={this.state.text} />
+      <Text textChangeHandler={this.textChangeHandler} text={this.state.text} /> 
+      {this.state.correctPass === true ? 
+      <div>
+        <p>You typed the right password!</p> 
+        <img src="https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&h=350" alt="puppy"/>
+      </div>
+      : null}
       </div>
    );
   } 
--- my-first-react-app/src/components/Text.js
+++ my-first-react-app/src/components/Text.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Text = (props) => {
 	return (
 		<div>
+			<h1>Hi, I'm a component. Can you guess the password?</h1>
+			<h6>The password is 'puppies', trust me.</h6>
 			<input type="text" value={props.text} onChange={props.textChangeHandler}/>
-			<p>{props.text}</p>
+			<h3>You're Updating the state here:</h3>
+			<h4>{props.text}</h4>
 		</div>	
 		)
 }
