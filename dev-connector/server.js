70b5eeac54c5815da50fa5cac304114a072d084e
Shaquille Haynes <nothintodoee@yahoo.com>
2018-08-08 08:16:52 -0400

    add mongoDB connection

--- dev-connector/server.js
+++ dev-connector/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const keys = require("./keys/keys");
 
 // initialize app
 const app = express();
+
+//establish mongoDB connection
+const db = keys.mongoURI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(res => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // test route
 app.get("/", (req, res) => {
