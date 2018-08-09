f2828c71afa0f2f4bdcf9cb8f326a92eb836ea2c
S-Haynes <39104316+S-Haynes@users.noreply.github.com>
2018-08-09 03:12:12 -0400

    Deleting File from Repo

--- dev-connector/config/keys.js
+++
@@ -1,7 +0,0 @@
-const keys = {
-  mongoURI:
-    "mongodb://shaynes:shaynes123@ds215502.mlab.com:15502/mern_devconnector",
-  secret: "asupersecretkeyforthissupercoolproject"
-};
-
-module.exports = keys;
