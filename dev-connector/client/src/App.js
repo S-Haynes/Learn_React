9b6441c6c2d62bd8542798abbe7f80cf455dc555
Shaquille Haynes <nothintodoee@yahoo.com>
2018-08-12 20:52:29 -0400

    add redux workflow to repo

--- dev-connector/client/src/App.js
+++ dev-connector/client/src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Layout/Navbar/Navbar";
+import Footer from "./components/Layout/Footer/Footer";
+import Landing from "./components/Layout/Landing/Landing";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Register from "./components/Auth/Register/Register";
+import Login from "./components/Auth/Login/Login";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+            </Switch>
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
